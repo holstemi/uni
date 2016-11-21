3459a5157801ea13b40d199da70417f454163134
izvest <eholsti@gmail.com>
2016-11-21 17:08:40 +0200

    demot 2 aloitettu

---
+++ aop/demot2/demo26.java
@@ -0,0 +1,69 @@
+public class demo26 {
+	
+	public static void main(String[] args){
+		
+		Scanner s = new Scanner(System.in);
+		System.out.println("Anna kaksi lukua:");
+		int a = s.nextInt();
+		if(a < 0){
+			System.out.println("Luvun t�ytyy olla positiivinen. Anna uusi luku:");
+			a = s.nextInt();			
+		}
+		
+		System.out.println("Kiitos! Anna seuraava luku:");
+		int b = s.nextInt();
+		
+		System.out.println(Math.sqrt(a));
+		System.out.println("----");
+		
+		int temp = 0;
+		temp = a;
+		a = b%a;
+		b = temp;
+		
+		if(a > 0){
+			temp = a;
+			a = b%a;
+			b = temp;
+		}
+		if(a > 0){
+			temp = a;
+			a = b%a;
+			b = temp;
+		}
+		if(a > 0){
+			temp = a;
+			a = b%a;
+			b = temp;
+		}
+		
+		temp = b;
+		
+		System.out.println(b);
+	
+		if(temp > 0){
+			System.out.println(--temp);
+		}
+		if(temp > 0){
+			int luku = temp;
+			luku = luku-1;
+			System.out.println(luku);
+		}
+		if(temp > 0){
+			temp = temp-2;
+			System.out.println(temp);
+		}
+		if(temp > 0){
+			int x = --temp;
+			System.out.println(x);
+			temp--;
+		}
+		if(temp > 0){
+			System.out.println(temp);			
+		}
+		if(temp > 0){
+			temp -= 1;
+			System.out.println(temp);
+		}
+	}
+}
---
+++ aop/demot2/demot_aio.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class demot_aio{
+	
+	public static void main(String[] args){
+		try{
+			Scanner re = new Scanner(System.in);
+			System.out.print("Valitse demotehtava: ");
+			int demo = re.nextInt();
+			re.nextLine();
+			System.out.println("------------------------");
+			if (demo == 1){
+				System.out.print("Syota kokonaisluku n: ");
+				int n = re.nextInt();
+				System.out.println("------------------------");
+				
+				public static int fibonacci(int n){
+					if (n <= 1){return n;}
+					else {return fibonacci(n-1) + fibonacci(n-2);}
+				}
+				
+				for (int i = 0; i <= n; i++){
+					System.out.print(fibonacci(i) + " ");
+					System.out.print(i*i + " ");
+				}
+			}
+			else if (demo == 2){
+				System.out.print("Syota merkkijono: ");
+				String m = re.nextLine();
+				System.out.println("------------------------");
+				
+			}
+			else if (demo == 3){
+				System.out.print("Syota pariton positiivinen kokonaisluku: ");
+				int k = re.nextInt();
+				System.out.println("------------------------");
+				if (k%2 != 0 && k > 0){
+					for (int i = 0; i <= k; i++){
+						for (int j = 0; j <= 2*i-1; j++){
+							System.out.print("*");
+						}
+					}
+				}else{System.out.println("Syottamasi luku ei ole parillinen ja/tai postiivinen kokonaisluku.");}
+			}
+			else if (demo == 4){
+				System.out.print("Syota merkkijono: ");
+				String s = re.nextLine();
+				System.out.println("------------------------");
+				
+			}
+			else if (demo == 5){
+				System.out.print("Syota merkkijono A: ");
+				String a = re.nextLine();
+				System.out.print("Syota merkkijono B: ");
+				String b = re.nextLine();
+				System.out.print("Syota merkkijono C: ");
+				String c = re.nextLine();
+				System.out.println("------------------------");
+				
+			}
+			else if (demo == 6){
+				System.out.print("Ks. tiedosto demo26.java");
+			}
+			else{System.out.println("Etsimaasi demoa ei loytynyt. Yrita uudelleen.");}
+			lukija.close();
+		}
+		catch(Exception e){System.out.println("Jokin meni pieleen, yrita uudelleen!");}
+	}
+}
