f262d199f48dc40ddc07754b4431813be11c959f
izvest <eholsti@gmail.com>
2016-11-29 13:22:28 +0200

    demo3 aloitus

---
+++ aop/demot3/demot3_aio.java
@@ -0,0 +1,52 @@
+mport java.util.Scanner;
+
+public class demot_aio{
+	
+	public static boolean onkoPalindromi(String m){
+		String g = "";
+		int ml = m.length();
+		//if (ml%2 = 0){
+			for (int i = ml; i > ml/2; i--){
+				g += m.charAt(i);
+			}
+		//}
+		if (m.substring(0,ml).equals(g)){
+			return true;
+		}
+		else{return false;}
+	}
+
+	public static void main(String[] args){
+		try{
+			Scanner re = new Scanner(System.in);
+			System.out.print("Valitse demotehtava: ");
+			int demo = re.nextInt();
+			re.nextLine();
+			System.out.println("------------------------");
+			if (demo == 1){
+				System.out.print("Syota merkkijono: ");
+				string s = re.nextLine();
+				System.out.println("------------------------");
+				System.out.println(onkoPalindromi(s);)
+			}
+			else if (demo == 2){
+				
+			}
+			else if (demo == 3){
+				System.out.print("Ks. tiedosto demo33.java");
+			}
+			else if (demo == 4){
+				
+			}
+			else if (demo == 5){
+				
+			}
+			else if (demo == 6){
+				
+			}
+			else{System.out.println("Etsimaasi demoa ei loytynyt. Yrita uudelleen.");}
+			re.close();
+		}
+		catch(Exception e){System.out.println("Jokin meni pieleen, yrita uudelleen!");}
+	}
+}
