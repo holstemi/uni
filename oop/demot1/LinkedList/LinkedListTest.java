c90664077eee41eefe9ce3d8e80e517b65048658
izvest <eholsti@gmail.com>
2017-01-25 21:49:35 +0200

    4 uudelleentehty ja ainakin lähes toiminnallinen

---
+++ oop/demot1/LinkedList/LinkedListTest.java
@@ -0,0 +1,20 @@
+public class LinkedListTest{
+	public static void main(String[] args) {
+
+    	LinkedList L = new LinkedList();
+    	L.add(1);
+    	L.show();
+    	L.add(2);
+    	L.show();
+    	L.add(3);
+    	L.show();
+    	L.deleteNodeWithData(2);
+    	L.show();
+    	L.deleteNodeAtIndex(3);
+    	L.show();
+    	L.deleteNodeAtIndex(1);
+    	L.show();
+}
+
+
+//Crediz for hjalp: https://gist.github.com/akshaynagpal/14ee8b54cd018bd05b2a3c2432c818dc
