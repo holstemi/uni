9be5dcee563c7e0dc09d9226a12ed0a14ba5d907
iikjl <iikka.luoma-aho@outlook.com>
2017-02-12 23:59:49 +0200

    Create Testi.java

---
+++ oop/demot3/2_Jalkapalllo/Testi.java
@@ -0,0 +1,18 @@
+public class Testi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stu
+		Jalkapallojoukkue a = new Jalkapallojoukkue("kerava");
+		Jalkapallonpelaaja c = new Jalkapallonpelaaja("Matti",32,"5");
+		Jalkapallonpelaaja g = new Jalkapallonpelaaja("Teppo",43,"6");
+		Jalkapallonpelaaja j = new Jalkapallonpelaaja("Pekka",3,"7");
+		j.asetaPalkka(200.98);
+		c.asetaPalkka(900.0);
+		g.asetaPalkka(29.0);
+		a.lisaaPelaaja(j);
+		a.lisaaPelaaja(g);
+		a.lisaaPelaaja(c);
+		a.tulostaPelaajatPalkka(2); // annettavan numeron on oltava <= jalkapallojoukkueen koko
+	}
+
+}
