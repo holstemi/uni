a2a7ea525687edded265343191b8e8872465ad35
iikjl <iikka.luoma-aho@outlook.com>
2017-02-13 00:06:34 +0200

    Update Hiomakone.java

--- oop/demot3/3_Tyokalupakki/Hiomakone.java
+++ oop/demot3/3_Tyokalupakki/Hiomakone.java
@@ -1,8 +1,8 @@
 public class Hiomakone extends Tyokalupakki{
 
 public Hiomakone(){
-	private String nimi = new String("Hiomakone");
-	tyokalut.add(<Hiomakone> hiomakone);
+	private String nimi = new String("Hiomakone");	
+	tyokalut.add(this);
 }
 public boolean onTeroitettavissa(){
   return false;
