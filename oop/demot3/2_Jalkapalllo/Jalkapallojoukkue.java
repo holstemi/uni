e61271142d91bdbf72bd2b42591690d4161e1658
iikjl <iikka.luoma-aho@outlook.com>
2017-02-12 23:56:31 +0200

    Update Jalkapallojoukkue.java

--- oop/demot3/2_Jalkapalllo/Jalkapallojoukkue.java
+++ oop/demot3/2_Jalkapalllo/Jalkapallojoukkue.java
@@ -1,3 +1,7 @@
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
+
 class Jalkapallojoukkue extends Joukkue{
 	final int MAX_PELAAJIA = 22;
 	String kotistadion;
@@ -8,10 +12,29 @@ class Jalkapallojoukkue extends Joukkue{
 	}
 	
 	public void lisaaPelaaja(String n, int num, String pp){
-		if(pelaajat.size < MAX_PELAAJIA){
-			Pelaaja uusiPelaaja = new Pelaaja(n,num,pp);
+		if(pelaajat.size() < MAX_PELAAJIA){
+			Jalkapallonpelaaja uusiPelaaja = new Jalkapallonpelaaja(n,num,pp);
 			pelaajat.add(uusiPelaaja);
 		}
 		else{System.out.println("Joukkue taynna");}
 	}
+	public void lisaaPelaaja(Jalkapallonpelaaja j) {
+		pelaajat.add(j);
+		
+	}
+	public void tulostaPelaajatPalkka(int n){
+	List<Jalkapallonpelaaja> p = new ArrayList<Jalkapallonpelaaja>();
+	for(int i = 0; i < pelaajat.size();i++){
+		p.add((Jalkapallonpelaaja)pelaajat.get(i));}
+	
+	Collections.sort(p);
+	for (int i = p.size(); i >=n;i--){   
+   System.out.println(p.get(i-1).toString());    
+  }
+	
+
+}
+
+	
+	
 }
