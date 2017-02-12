c399b1ce4a3c40daa4b68a16f17cd101931f2221
izvest <eholsti@gmail.com>
2017-02-12 22:06:16 +0200

    #melkeintoimii

--- oop/demot3/4_BinaryTree/BSTest.java
+++ oop/demot3/4_BinaryTree/BSTest.java
@@ -2,19 +2,19 @@ public class BSTest{
 	public static void main(String[] args) {
 
     	BST<Integer> B = new BST<Integer>();
-    	B.add(1);
+    	B.add(1,1);
     	B.print();
-    	B.add(5);
+    	B.add(5,5);
     	B.print();
-    	B.add(3);
+    	B.add(3,3);
     	B.print();
 		B.find(3);
-		B.add(42);
-		B.add(2021);
-		B.add(364);
-		B.add(34);
-		B.add(769);
-		B.add(6);
+		B.add(42,42);
+		B.add(2021,2021);
+		B.add(364,364);
+		B.add(34,34);
+		B.add(769,769);
+		B.add(6,6);
     	B.print();
 	}
 }
