84a5926f6d8d1ffe2232478c2e82721987c7ea64
iikjl <iikka.luoma-aho@outlook.com>
2017-02-12 20:57:28 +0200

    Update Kirves.java

--- oop/demot3/3_Tyokalupakki/Kirves.java
+++ oop/demot3/3_Tyokalupakki/Kirves.java
@@ -1,3 +1,13 @@
 public class Kirves extends Tyokalupakki{
+  public boolean onTeroitettavissa(){
+  return true;
+  }
+	public boolean onLadattavissa(){
+  return false;
+  }
+	public String aani(){
+	return "Klops";
+	}
+}
 
 }
