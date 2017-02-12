87cc95d82d3734fecfd98ad894cd17d679548d90
iikjl <iikka.luoma-aho@outlook.com>
2017-02-12 21:00:11 +0200

    Update Saha.java

--- oop/demot3/3_Tyokalupakki/Saha.java
+++ oop/demot3/3_Tyokalupakki/Saha.java
@@ -1,3 +1,13 @@
 public class Saha extends Tyokalupakki{
+  public boolean onTeroitettavissa(){
+  return true;
+  }
+	public boolean onLadattavissa(){
+  return false;
+  }
+	public String aani(){
+	return "Krunch";
+	}
+}
 
 }
