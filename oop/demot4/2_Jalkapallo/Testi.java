41be3502a340dc2c58f4e2b0bb9d7ee93c678c33
iikjl <iikka.luoma-aho@outlook.com>
2017-02-19 17:11:31 +0200

    Update Testi.java
    tehtava tehty

--- oop/demot4/2_Jalkapallo/Testi.java
+++ oop/demot4/2_Jalkapallo/Testi.java
@@ -1,17 +1,25 @@
+
 public class Testi {
 
 	public static void main(String[] args) {
 		Jalkapallojoukkue a = new Jalkapallojoukkue("kerava");
-		Jalkapallonpelaaja c = new Jalkapallonpelaaja("Matti",32,"5");
-		Jalkapallonpelaaja g = new Jalkapallonpelaaja("Teppo",43,"6");
-		Jalkapallonpelaaja j = new Jalkapallonpelaaja("Pekka",3,"7");
-		j.asetaPalkka(200.98);
+		Jalkapallonpelaaja b = new Jalkapallonpelaaja("Matti",32,"5");
+		Jalkapallonpelaaja c = new Jalkapallonpelaaja("Teppo",43,"6");
+		Jalkapallonpelaaja d = new Jalkapallonpelaaja("Pekka",3,"7");
+		a.asetaPALKKAKATTO(1300);
+		b.asetaPalkka(200.98);
 		c.asetaPalkka(900.0);
-		g.asetaPalkka(29.0);
-		a.lisaaPelaaja(j);
-		a.lisaaPelaaja(g);
+		d.asetaPalkka(29.0);
+		a.lisaaPelaaja(b);
+		a.lisaaPelaaja(d);
 		a.lisaaPelaaja(c);
-		a.tulostaPelaajatPalkka(2); // annettavan numeron on oltava <= jalkapallojoukkueen koko
+		a.tulostaPelaajatPalkka(2); 
+		Jalkapallonpelaaja e = new Jalkapallonpelaaja("Seppo",3,"7");
+		e.asetaPalkka(300);
+		a.tulostaBudjetti();
+		a.lisaaPelaaja(e);
+		
+		
 	}
 
 }
