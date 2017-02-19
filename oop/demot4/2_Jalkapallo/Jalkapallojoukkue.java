d56da4bee69feca78fdce15c13715dd2c4a72d2b
iikjl <iikka.luoma-aho@outlook.com>
2017-02-19 17:12:04 +0200

    Muokattu ohjeiden mukaan

--- oop/demot4/2_Jalkapallo/Jalkapallojoukkue.java
+++ oop/demot4/2_Jalkapallo/Jalkapallojoukkue.java
@@ -4,11 +4,13 @@ import java.util.ArrayList;
 
 class Jalkapallojoukkue extends Joukkue{
 	final int MAX_PELAAJIA = 22;
-	String kotistadion;
+	 private String kotistadion;
+	 private double budjetti;
+	 
 	
 	Jalkapallojoukkue(String k){
 		super();
-		kotistadion = k;
+		this.asetaKotistadion(k);
 	}
 	
 	public void lisaaPelaaja(String n, int num, String pp){
@@ -20,7 +22,12 @@ class Jalkapallojoukkue extends Joukkue{
 	}
 	public void lisaaPelaaja(Jalkapallonpelaaja j) {
 		pelaajat.add(j);
-		
+		try {
+			lisaaBudjetti(j.annaPalkka());
+		} catch (BudjettiYlitetty e) {
+			
+			System.out.println("VIRHE: "+e.getMessage());
+		}
 	}
 	public void tulostaPelaajatPalkka(int n){
 	List<Jalkapallonpelaaja> p = new ArrayList<Jalkapallonpelaaja>();
@@ -35,6 +42,27 @@ class Jalkapallojoukkue extends Joukkue{
 
 }
 
+
+	public String annaKotistadion() {
+		return kotistadion;
+	}
+
+	public void asetaKotistadion(String kotistadion) {
+		this.kotistadion = kotistadion;
+	}
+
+	public void lisaaBudjetti(double a ) throws BudjettiYlitetty{
+		if(budjetti+a > Joukkue.PALKKAKATTO){
+			throw new BudjettiYlitetty("Ylittaa palkkakaton");
+		}
+		budjetti += a;
+			
+			
+	}
+	public void tulostaBudjetti(){
+		System.out.println(budjetti);
+	}
 	
+
 	
 }
