1da267d4f2724e4d7597b50fca49673879b1e6d3
Emil <izvest@users.noreply.github.com>
2017-02-20 10:23:34 +0200

    fixed?

--- oop/demot4/3_BST/BSTest.java
+++ oop/demot4/3_BST/BSTest.java
@@ -2,19 +2,19 @@ public class BSTest{
 	public static void main(String[] args) {
 
     	BST<Integer> b = new BST<Integer>();
-    	b.add(1,1);
+    	b.add(1);
     	b.print();
-    	b.add(5,5);
+    	b.add(5);
     	b.print();
-    	b.add(3,3);
+    	b.add(3);
     	b.print();
-		b.find(3);
-		b.add(42,42);
-		b.add(2021,2021);
-		b.add(364,364);
-		b.add(34,34);
-		b.add(769,769);
-		b.add(6,6);
+	b.find(3);
+	b.add(42);
+	b.add(2021);
+	b.add(364);
+	b.add(34);
+	b.add(769);
+	b.add(6);
     	b.print();
 	}
 }
